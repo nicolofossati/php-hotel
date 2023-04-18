335668e9c087adedce3ead749ae7e9f566b470b9
nicolo fossati <nicofossa01@gmail.com>
2023-04-18 14:08:48 +0200

    aggiunta bootstrap e creazione tabella dinamica

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
     <title>Hotels</title>
 </head>
 
@@ -49,18 +54,44 @@
         ],
 
     ];
-    foreach ($hotels as $hotel) {
-        foreach ($hotel as $key => $value) {
-            echo $key . ": " . $value . "<br/>";
-        }
-        echo "<br /><br /><br />";
-    }
-
     ?>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance to center</th>
+            </tr>
+        </thead> <!-- Apertura table e titoli -->
+
+        <tbody>
+            <?php
+
+            foreach ($hotels as $hotel) {
+                ?>
+                <tr>
+                    <?php
+                    foreach ($hotel as $value) {
+                        echo "<td>" . $value . "</td>";
+                    }
+                    ?>
+                </tr>
+                <?php
+            }
+
+            ?>
+
+        </tbody>
+    </table> <!-- chiusura table -->
 
 
 
-
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
